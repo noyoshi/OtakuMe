1a19d018f37512f183e9575e72f9963cf53eb5c4
silver <noahcy117@gmail.com>
2017-11-25 22:13:50 -0500

    updates

--- mysite/project/get_info.py
+++ mysite/project/get_info.py
@@ -21,11 +21,15 @@ def name_to_id(name):
 
     # c.execute('SELECT id FROM anime WHERE title=?', t)
     # print c.fetchone()
-    for row in c.execute('SELECT * FROM anime'):
-        # print row[0]
-        # print type(row[0])
-        if row[0] == name1:
-            return int(row[1])
+
+    # for row in c.execute('SELECT * FROM anime'):
+    #     # print row[0]
+    #     # print type(row[0])
+    #     if row[0] == name1:
+    #         return int(row[1])
+
+    # for row in c.execute('SELECT id FROM anime WHERE id =*?*', t):
+
 
 def get_anime_info(name):
     name1 = name.encode('utf-8')
@@ -35,13 +39,21 @@ def get_anime_info(name):
     descriptions = []
     c = conn.cursor()
     ids = []
+    t = (name1, )
+
     # t = (name1, )
     # c.execute('SELECT id FROM anime WHERE title=?', t)
     # _id = str(c.fetchone()[0])
+
     for row in c.execute('SELECT * FROM anime'):
         x = str(row[0])
-        if name in x:
+        if name in x or name == x:
             ids.append(str(row[1]))
+
+    # c.execute('SELECT id FROM anime WHERE id =*?', t)
+    # ids.append(str(c.fetchall()))
+
+
     for _id in ids:
         bad = False
         url="https://www.animenewsnetwork.com/encyclopedia/api.xml?anime=" + _id
@@ -61,8 +73,8 @@ def get_anime_info(name):
         plot_summary = ""
         pic = ""
         title = ""
-        # for item in x:
-        #     print item.text
+#TODO Add in more functionality-> website links, episode lists, run years,
+# episode names,
         for item in page_soup.findAll(type="Ending Theme"):
             songs[1].append(item.text.encode('utf-8'))
         for item in page_soup.findAll(type="Opening Theme"):
@@ -79,6 +91,7 @@ def get_anime_info(name):
             pic= item.find('img')['src']
         for item in page_soup.findAll(type="Main title"):
             title = item.text.encode('utf-8')
+        crunchy = "-".join(title.split())
         # print pic
         # print genres
         # print themes
@@ -89,7 +102,7 @@ def get_anime_info(name):
             if show['title'] == title:
                 bad = True
         if not bad:
-            descriptions.append({'image': pic, 'title':title, 'genres': genres, 'themes': themes, 'songs': songs, 'summary': plot_summary})
+            descriptions.append({'crunchy': crunchy, 'image': pic, 'title':title, 'genres': genres, 'themes': themes, 'songs': songs, 'summary': plot_summary})
     return descriptions
 #for item in page_soup.findAll("item"):
 
